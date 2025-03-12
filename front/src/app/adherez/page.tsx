3fd153f47c879c18c69a35bae9e044aaeec17f59
Tommy31 <totolamothe65@gmail.com>
2025-03-12 14:01:18 +0100

    feat: fetch data from strapi

--- front/src/app/adherez/page.tsx
+++ front/src/app/adherez/page.tsx
@@ -1,3 +1,24 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {BackendClient} from "@/utils/Client";
+
 export default function Join() {
-    return <h1>Adhérez</h1>;
+    const [join, setJoin] = useState({description: '', titre: '', updatedAt: null});
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await BackendClient.request({path: '/api/join'}).then(res => res.json());
+
+            setJoin(data);
+        })();
+    }, []);
+
+    return (
+        <>
+            <h1>{join.titre}</h1>
+            <h6>Dernière mise à jour : {join.updatedAt}</h6>
+            <p>{join.description}</p>
+        </>
+    );
 }
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import {useEffect, useState} from "react";
+import {BackendClient} from "@/utils/Client";
+
 export default function Home() {
-  return <h1>HomePage</h1>;
+    const [events, setEvents] = useState([]);
+
+    const fetchEvents = async (): Promise<void> => {
+        setEvents(await BackendClient.request({ path: '/api/evenements' }).then(res => res.json()).then(({data}) => data));
+    }
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    return (
+        <>
+            <h1>HomePage</h1>
+            <p>Salut ça va</p>
+
+            {events?.map(event => <div key={event.id}>{event.titre}</div>)}
+        </>
+    );
 }
